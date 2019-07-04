f3ee7cde09da9b5218c972e0a4aec84b10cbfdf1
Ihor Salnikov <ihor.salnikov@gmail.com>
2019-07-04 18:58:09 +0200

    Initial commit

---
+++ app/users/user.controller.ts
@@ -0,0 +1,19 @@
+import {controller, httpPost, interfaces, requestBody, response} from 'inversify-express-utils';
+import {Response} from 'express';
+
+@controller('/user')
+export class UserController implements interfaces.Controller {
+
+  @httpPost('/login')
+  public login(@response() res: Response,
+               @requestBody() params?: {name: string, password: string}) {
+    return res.json({huj: true});
+  }
+
+  @httpPost('/register')
+  public register(@response() res: Response) {
+    return res.
+  }
+
+
+}
